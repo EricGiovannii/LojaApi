125c167dc228775e59532fe611e2a58ed1980be7
unknown <Administrador@GAB-9028030.PORTOVELHO.RO.GOV.BR>
2026-09-10 12:41:30 -0400

    Testa cabeçalho CORS manualmente

--- lib/loja_api_web/endpoint.ex
+++ lib/loja_api_web/endpoint.ex
@@ -42,6 +42,12 @@ defmodule LojaApiWeb.Endpoint do
 
   plug Plug.RequestId
 
+  # ============================================
+  # TESTE TEMPORÁRIO DE CORS
+  # ============================================
+
+  plug :put_cors_test_header
+
   plug CORSPlug,
     origin: "*",
     credentials: false,
@@ -77,4 +83,16 @@ defmodule LojaApiWeb.Endpoint do
   plug Plug.Head
   plug Plug.Session, @session_options
   plug LojaApiWeb.Router
+
+  # ============================================
+  # TESTE TEMPORÁRIO DE CORS
+  # ============================================
+
+  defp put_cors_test_header(conn, _opts) do
+    Plug.Conn.put_resp_header(
+      conn,
+      "access-control-allow-origin",
+      "*"
+    )
+  end
 end
